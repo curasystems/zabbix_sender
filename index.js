aef73a185736e62aefc6b47bf3785ed9df04daf1
Adrian Ortmann <adrian.ortmann@online.de>
2013-09-01 13:42:33 +0200

    its working with startSender command, trying to do it in two steps. First set options after you can start

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var zabbixSender = require('./lib/sender');
 var configPath = 'C:\\Development\\private\\aortmann\\zabbix\\zabbix_agentd.conf';
 
 var itemKeysAndValuesToSend = {
-	'receive.folder': 1,
+	'receive.folder': 0,
 	'why.dont': 0,
 	'another.key': 1
 };
 
-zabbixSender.senderOptions(configPath, itemKeysAndValuesToSend);
+// zabbixSender.senderOptions(configPath, itemKeysAndValuesToSend);
+zabbixSender.startSender(configPath, itemKeysAndValuesToSend);
